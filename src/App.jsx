2b4b954da2e3703b6804d3046c8c2ba5892237aa
akhatarmourad <mourad.akhatar01@gmail.com>
2024-09-25 16:41:52 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { UseStateHook } from './hooks';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/* useState Hook */}
+      <UseStateHook />
+
+      <hr />
+    </div>
+  )
+}
+
+export default App
---
+++ src/hooks/UseStateHook.jsx
@@ -0,0 +1,9 @@
+import  { useState } from "react";
+
+export default function UseStateHook() {
+  return (
+    <div>
+        <button>Update Counter</button>
+    </div>
+  )
+}
