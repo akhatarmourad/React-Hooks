b1828d321f8f5e944624d21fcf75bb8f5b70c8bd
akhatarmourad <mourad.akhatar01@gmail.com>
2024-09-25 17:10:34 +0100

    Adding Use State

--- src/hooks/UseStateHook.jsx
+++ src/hooks/UseStateHook.jsx
@@ -1,9 +1,14 @@
 import  { useState } from "react";
 
 export default function UseStateHook() {
+
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div>
-        <button>Update Counter</button>
+    <div className="bg-gray-300 p-5 rounded-xl flex items-center gap-5">
+        <span>Counter Value : <span>{counter}</span></span>
+        <button onClick={() => setCounter(counter - 1)} className="py-1 px-2 outline-none bg-sky-500 text-white rounded-lg">Increment Counter</button>
+        <button onClick={() => setCounter(counter + 1)} className="py-1 px-2 outline-none bg-indigo-500 text-white rounded-lg">Decrement Counter</button>
     </div>
   )
 }
